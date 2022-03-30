045d61e180da456f34306aa70968f9364a000963
Jadslim <71816341+Jadslim@users.noreply.github.com>
2022-03-30 16:05:12 +0200

    Update proj.c

--- proj.c
+++ proj.c
@@ -48,58 +48,48 @@ long modpow_naive(long a, long m, long n){
 }    
 
 
-long modpow(long a, long m, long n){
-    /* realise l'exponentielle modulo n de maniere recursive
-        */
-    if ( m== 0) return 0;
-    
-    else if (m % 2 ==0) {
-        long b = modpow(a,m/2,n);
-        return b*b%n;
-    }
-    else{
-        long b = modpow(a,m-1,n);
-        return b*a%n;
-    }
-    
+long modpow(long a,long m,long n){
+	if(m==1){
+		return a%n;
+	}
+
+	if( m % 2 == 0){
+		long b= modpow(a, m/2, n);
+		return b* b % n;
+	}
+	else
+	{
+		long b=modpow(a,floor(m/2),n);
+		return a * b * b % n;
+	}
 }
 
-long random_prime_number(int low_size, int up_size, int k){
-    //retourne un nombre premier de taille comprise entre low size et up size
-    long low =(long)(power(2,low_size-1));
-    long up = (long)(power(2,up_size)-1);
-    long res = rand_long(low,up);
-    while(is_prime_miller(res,k)==0){
-        res = rand_long(low,up);
-    }
-    if (is_prime_naive(res)==0){
-        printf("non premier\n");
-    }
-    return res;
+
+
+int witness(long a,long b,long d,long p) 
+{
+	long x = modpow(a,d,p);
+	if ( x == 1)
+	{
+		return 0;
+	}
+	for (long i = 0; i < b;i++)
+	{
+		if ( x == p-1)
+		{
+			return 0;
+ 		}
+		//printf("nombre d'iterations:%lu\n",i);
+	x=modpow(x ,2 ,p);
+	}
+	return 1;
 }
 
-int witness(long a, long b,long d,long p){
-    /* renvoie 1 si a est un temoin de Miller ; O sinon
-        */
-    long x = modpow(a,d,p);
-    if(x==1){
-        return 0;
-    }
-    for(long i=0; i < b; i++){
-        if(x ==p-1){
-            return 0;
-        }
-        x = modpow(x,2,p);
-    }
-    return 1;
+long rand_long(long low,long up)
+{
+	return rand()%(up-low+1)+low;
 }
-long rand_long(long low,long up){ 
-    /* renvoie un entier generes aleatoirement entre low et up
-        */
 
-    long val =  (long)rand() % ((up-low+1))-low;
-    return val;
-}
 long power(long a,long b){
     long cpt=1;
     for(int i = 0;i<=b;i++){
@@ -108,35 +98,49 @@ long power(long a,long b){
     }
     return cpt;
 }
-int is_prime_miller(long p, int k){
-    /* realise le test de miller-Rabin */
-    if (p == 2){
-        return 1;
-    }
-    if(!(p & 1) || p <= 1){
-        return 0;
+int is_prime_miller(long p ,int k)
+{
+	if (p == 2) 
+	{
+		return 1;
+	}
+	if(!( p & 1) || p <= 1) //on verifie que p est impair et different de 1
+	{ 
+		return 0;
+ 	}
+	//on determine b et d :
+	long b = 0;
+	long d = p - 1;
+	while(!( d & 1)) //tant que d n’est pas impair
+	{ 
+		d = d /2;
+ 		b = b+1;
+	}
+	// On genere k valeurs pour a, et on teste si c’est un temoin :
+	long a;
+	int i;
+	for( i = 0; i < k ; i++)
+	{
+		a =rand_long(2,p -1);
+	
+		if(witness(a,b,d,p)) 
+		{	
+			return 0;
+		}
+		//printf("valeur de witness apres %d tour: %d\n",i,witness(a,b,d,p));
+	
+	}
+	return 1;
+}
 
-    }
-    // on determine b et d
-    long b = 0;
-    long d = p-1;
-    while(!(d & 1)){//tant que d n’est pas impair
-    d = d / 2;
-    b+=1;
-    }
-    // On genere k valeurs pour a, et on teste si c’est un temoin :
-    long a;
-    int i;
-    for(i=0;i<k;i++){
-        a = rand_long(2, p-1);
-        if(witness(a,b,d,p)){
-            return 0;
-        }
-    }
-    return 1;
-}
     
-/*int main(){
-    return 0;
+long random_prime_number(int low_size, int up_size, int k)
+{  
+	long p=rand_long(pow(2,low_size-1),pow(2,up_size)-1);
+
+	while(is_prime_miller(p,k)==0)
+	{
+		p=rand_long(pow(2,low_size-1),pow(2,up_size)-1);
+  	}
+  	return p;
 }
-*/
